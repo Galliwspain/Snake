7c5077fb22f91f9215751202f279bedaed8c27f9
Galliwspain <isincere@yandex.ru>
2015-12-01 22:52:18 +0600

    add new class and function inside

---
+++ Snake/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw() {
+            Console.SetCursorPosition(x, y);//установить курсор на позицию
+            Console.Write(sym);
+        }
+    }
+}
